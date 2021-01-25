66ab8b9c3fa821f9480fe8c60d8fa3bae9294a71
鲱鱼 <lf18234129242@163.com>
2021-01-25 15:32:59 +0800

    rem适配

--- js/base.js
+++ js/base.js
@@ -1,10 +1,10 @@
 (function (doc, win) {
-  var docEl = doc.documentElement,
 	resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
 	recalc = function () {
-	  var clientWidth = docEl.clientWidth;
-	  if (!clientWidth) return;
-	  docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+	  var clientWidth = doc.documentElement.clientWidth
+		|| doc.body.clientWidth || window.innerWidth;
+		if (!clientWidth || clientWidth > 396) return;
+	  doc.documentElement.style.fontSize = 100 * (clientWidth / 375) + 'px';
 	};
   if (!doc.addEventListener) return;
   win.addEventListener(resizeEvt, recalc, false);
